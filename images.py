fa5513da8cfae9c0f6debb3290ee5fd357481b6a
Angela Crepaldi <angelacrepaldi@gmail.com>
2019-10-31 23:52:37 -0300

    Improvements to web scrape.

--- images.py
+++ images.py
@@ -2,21 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 from PIL import Image
 from io import BytesIO
+import os
 # import wtf # for Flask
 
-search = input("Search for:")
-params = {"q": search}
-r = requests.get("https://www.bing.com/images/search", params=params)
+def StartSearch():
+    search = input("Search for:")
+    params = {"q": search}
+    r = requests.get("https://www.bing.com/images/search", params=params)
+    dir_name = search.replace(" ", "_").lower()
 
-soup = BeautifulSoup(r.text, "html.parser")
-links = soup.findAll("a", {"class": "thumb"})
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
 
-for item in links:
-    img_ob = requests.get(item.attrs["href"])
-    print("Getting ", item.attrs["href"])
-    title = item.attrs["href"].split("/")[-1]
-    # title = title.split("?")[0]
-    print(title)
-    img = Image.open(BytesIO(img_ob.content))
-    print(img.format)
-    img.save("./scraped_images/" + title, img.format)
+    soup = BeautifulSoup(r.text, "html.parser")
+    links = soup.findAll("a", {"class": "thumb"})
+
+    for item in links:
+        try:
+            img_ob = requests.get(item.attrs["href"])
+            print("Getting ", item.attrs["href"])
+            title = item.attrs["href"].split("/")[-1]
+            # title = title.split("?")[0]
+            # print(title)
+            try:
+                img = Image.open(BytesIO(img_ob.content))
+                # print(img.format)
+                # img.save("./scraped_images/" + title, img.format)
+                img.save("./" + dir_name + "/" + title, img.format)
+            except:
+                print("Could not save image.")
+        except:
+            print("Could not request image.")
+
+    StartSearch()
+
+StartSearch()
