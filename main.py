d00991b0d3b59014f4d823df3f1f359f200d1646
Angela Crepaldi <angelacrepaldi@gmail.com>
2019-10-31 22:35:05 -0300

    Parsing soup.

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
-search = input("Enter search terms:")
-params = {"q=": search}
+search = input("Search for:")
+params = {"q": search}
 r = requests.get("https://www.bing.com/search", params=params)
 
 soup = BeautifulSoup(r.text, "html.parser")
-print(soup.prettify())
+#print(soup.prettify())
+results = soup.find("ol", {"id": "b_results"})
+
+links = results.findAll("li", {"class": "b_algo"})
+#print(links)
+
+for item in links:
+    item_text = item.find("a").text
+    item_href = item.find("a").attrs["href"]
+
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
